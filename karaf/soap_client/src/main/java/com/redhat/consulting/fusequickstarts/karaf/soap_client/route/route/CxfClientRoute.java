f9ff27d995e595c9d84f3f546be8ef651570ce83
Bryan Saunders <bsaunder@redhat.com>
2015-11-19 15:13:36 -0500

    Added Soap Client

--- karaf/soap_client/src/main/java/com/redhat/consulting/fusequickstarts/karaf/soap_client/route/route/CxfClientRoute.java
+++ karaf/soap_client/src/main/java/com/redhat/consulting/fusequickstarts/karaf/soap_client/route/route/CxfClientRoute.java
@@ -1,21 +1,31 @@
 package com.redhat.consulting.fusequickstarts.karaf.soap_client.route.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.SoapJaxbDataFormat;
 
 public class CxfClientRoute extends RouteBuilder{
+    
+    private static final String ENDPOINT = "http://www.webservicex.net/CurrencyConvertor.asmx";
+    private static final String WSDL = "http://www.webservicex.net/CurrencyConvertor.asmx?WSDL";
+    private static final String SERVICE_NAME = "{http://www.webserviceX.NET/}CurrencyConvertor";
+    private static final String ENDPOINT_NAME = "{http://www.webserviceX.NET/}CurrencyConvertorSoap";
 
     @Override
     public void configure() throws Exception {
         
+        SoapJaxbDataFormat jaxbDataFormat = new SoapJaxbDataFormat("net.webservicex");
+        
         // Process the Requests coming in to the Endpoint
-        from("cxf:bean:customerOrderEndpoint")
-            .log("Got a SOAP Message: ${body}")
-            .choice()
-                .when(simple("${in.header.operationName} == 'placeOrder'"))
-                    .to("direct:placeOrder")
-                .when(simple("${in.header.operationName} == 'getOrder'"))
-                    .to("direct:getOrder")
-            .endChoice();
+        from("timer://soapClientTimer?fixedRate=true&period=5000&repeatCount=2")
+            .setHeader("fromCurrency",simple("USD"))
+            .setHeader("toCurrency",simple("EUR"))
+            .bean(CurrencyConversionRequestFactory.class, "createNewRequest")
+            .marshal(jaxbDataFormat)
+            .log("Making SOAP Call: ${body}")
+            //.to("cxf:bean:cxfClientEndpoint?dataFormat=MESSAGE")
+            .to("cxf://" + ENDPOINT + "?dataFormat=MESSAGE&wsdlURL=" + WSDL + "&serviceName="
+                   + SERVICE_NAME + "&portName=" + ENDPOINT_NAME)
+            .log("SOAP Response: ${body}");
         
     }
 
