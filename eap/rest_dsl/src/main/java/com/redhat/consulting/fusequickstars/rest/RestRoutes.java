8ee3b1f9112b12da6bbbf88dedd44f166b2df893
Leandro Beretta <lea.beretta@gmail.com>
2016-02-24 11:14:20 -0300

    Se crean los servicios REST en Rest DSL

---
+++ eap/rest_dsl/src/main/java/com/redhat/consulting/fusequickstars/rest/RestRoutes.java
@@ -0,0 +1,44 @@
+package com.redhat.consulting.fusequickstars.rest;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.ContextName;
+import org.apache.camel.model.rest.RestBindingMode;
+
+@ContextName("rest-dsl")
+public class RestRoutes extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        restConfiguration().component("servlet").bindingMode(RestBindingMode.json);
+
+        rest("/user")
+                .get().produces("application/json").to("direct:getUsers")
+                .get("/{id}").produces("application/json").to("direct:getUser")
+                .post().consumes("application/json").type(User.class).to("direct:createUser")
+                .put().type(User.class).to("direct:updateUser")
+                .delete("/{id}").to("direct:deleteUser");
+
+        from("direct:getUsers")
+                .log("GET /user request received!")
+                .to("bean:usersBean?method=getUsers");
+
+        from("direct:getUser")
+                .log("GET /user/${header.id} request received!")
+                .setBody(simple("${header.id}"))
+                .to("bean:usersBean?method=getUser");
+
+        from("direct:createUser")
+                .log("POST /user/ request received!")
+                .to("bean:usersBean?method=createUser");
+
+        from("direct:updateUser")
+                .log("PUT /user/ request received!")
+                .to("bean:usersBean?method=updateUser");
+
+        from("direct:deleteUser")
+                .log("DELETE /user/${header.id} request received!")
+                .setBody(simple("${header.id}"))
+                .to("bean:usersBean?method=deleteUser");
+    }
+}
+
