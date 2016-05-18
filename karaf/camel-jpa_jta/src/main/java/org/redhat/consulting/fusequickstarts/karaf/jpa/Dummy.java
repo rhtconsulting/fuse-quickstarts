6afab3b9bff4aa58228ae82b4a548d4e80f112d4
Paolo Antinori <pantinor@redhat.com>
2016-05-18 09:44:17 +0200

    Added camel-jpa + jta transaction example

---
+++ karaf/camel-jpa_jta/src/main/java/org/redhat/consulting/fusequickstarts/karaf/jpa/Dummy.java
@@ -0,0 +1,11 @@
+package org.redhat.consulting.fusequickstarts.karaf.jpa;
+
+/**
+ * Created by benja on 4/29/16.
+ */
+public class Dummy {
+
+  public void crash() {
+    throw new RuntimeException();
+  }
+}
